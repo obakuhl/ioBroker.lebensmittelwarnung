06ca7cbf94c332f6c60b87d6b6e7c8fdb26b1120
Ben <bmfk_m@yahoo.de>
2024-08-16 14:51:58 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -48,44 +48,9 @@ async function main() {
 	// adapter.config:
 	adapter.log.info('config update_interval: ' + adapter.config.update_interval);
 	adapter.log.info('config states: ' + adapter.config.states);
+		adapter.setState('info.connection', false, true);
 
-	/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-	*/
-	await adapter.setObjectNotExistsAsync('testVariable', {
-		type: 'state',
-		common: {
-			name: 'testVariable',
-			type: 'boolean',
-			role: 'indicator',
-			read: true,
-			write: true,
-		},
-		native: {},
-	});
 
-	// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-	adapter.subscribeStates('testVariable');
-	// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-	// adapter.subscribeStates('lights.*');
-	// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-	// adapter.subscribeStates('*');
-
-	/*
-		setState examples
-		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-	*/
-	// the variable testVariable is set to true as command (ack=false)
-	await adapter.setStateAsync('testVariable', true);
-
-	// same thing, but the value is flagged "ack"
-	// ack should be always set to true if the value is received from or acknowledged from the target system
-	await adapter.setStateAsync('testVariable', { val: true, ack: true });
-
-	// same thing, but the state is deleted after 30s (getState will return null afterwards)
-	await adapter.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
 }
 
 // If started as allInOne/compact mode => return function to create instance
