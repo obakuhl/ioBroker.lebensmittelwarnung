b53373804920d8213c28d421a8b92d9dfb6a88a7
Ben <bmfk_m@yahoo.de>
2024-08-16 15:05:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 'use strict';
 
-/*
- * Created with @iobroker/create-adapter v2.3.0
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
-const utils = require('@iobroker/adapter-core');
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
+const parseString = require('xml2js').parseString;
 
 /**
  * The adapter instance
@@ -17,135 +9,299 @@ const utils = require('@iobroker/adapter-core');
  */
 let adapter;
 
+const objectsInitialized = {};
+
+//let foodJSONDP = '0_userdata.0.Lebensmittel.Warnung.json'
+//let foodHTMLDP = '0_userdata.0.Lebensmittel.Warnung.html'
+//let selectedCountryDP = '0_userdata.0.Lebensmittel.Warnung.country'
+
+let contact = 'https://www.lebensmittelwarnung.de/DE/Service/Kontakt/kontakt_node.html'
+let foodWarningRSS =  'https://www.lebensmittelwarnung.de/___LMW-Redaktion/RSSNewsfeed/Functions/RssFeeds/rssnewsfeed_Alle_DE.xml'
+// 'https://www.lebensmittelwarnung.de/bvl-lmw-de/opensaga/feed/alle/alle_bundeslaender.rss'
+let maxEntries = 20;
+
 /**
  * Starts the adapter instance
  * @param {Partial<utils.AdapterOptions>} [options]
  */
 function startAdapter(options) {
-	// Create the adapter and define its methods
-	return adapter = utils.adapter(Object.assign({}, options, {
-		name: 'lebensmittelwarnung',
-
-		// The ready callback is called when databases are connected and adapter received configuration.
-		// start here!
-		ready: main, // Main method defined below for readability
-
-		// is called when adapter shuts down - callback has to be called under any circumstances!
-		unload: (callback) => {
-			try {
-				// Here you must clear all timeouts or intervals that may still be active
-				// clearTimeout(timeout1);
-				// clearTimeout(timeout2);
-				// ...
-				// clearInterval(interval1);
-
-				callback();
-			} catch (e) {
-				callback();
-			}
-		},
-
-		// If you need to react to object changes, uncomment the following method.
-		// You also need to subscribe to the objects with `adapter.subscribeObjects`, similar to `adapter.subscribeStates`.
-		// objectChange: (id, obj) => {
-		// 	if (obj) {
-		// 		// The object was changed
-		// 		adapter.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-		// 	} else {
-		// 		// The object was deleted
-		// 		adapter.log.info(`object ${id} deleted`);
-		// 	}
-		// },
-
-		// is called if a subscribed state changes
-		stateChange: (id, state) => {
-			if (state) {
-				// The state was changed
-				adapter.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-			} else {
-				// The state was deleted
-				adapter.log.info(`state ${id} deleted`);
-			}
-		},
-
-		// If you need to accept messages in your adapter, uncomment the following block.
-		// /**
-		//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-		//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-		//  */
-		// message: (obj) => {
-		// 	if (typeof obj === 'object' && obj.message) {
-		// 		if (obj.command === 'send') {
-		// 			// e.g. send email or pushover or whatever
-		// 			adapter.log.info('send command');
-
-		// 			// Send response in callback if required
-		// 			if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-		// 		}
-		// 	}
-		// },
-	}));
+	    adapter = new utils.Adapter(options);
+	    return adapter;
 }
 
-async function main() {
+function main() {
+	adapter.setState('info.connection', false, true);
+	
+	(async() => {
+        try {
+			adapter.log.debug(`hello`);		
+			await handleRequest();
+        } catch (err) {
+			adapter.log.info(`Could not process request: ${err}`);
+			return;
+            }
+	})();
+	
+	getData();
 
-	// The adapters config (in the instance object everything under the attribute "native") is accessible via
-	// adapter.config:
-	adapter.log.info('config update_interval: ' + adapter.config.update_interval);
-	adapter.log.info('config states: ' + adapter.config.states);
+	if(existsState(foodJSONDP)){
+	    getData()
+	} else{
+	    setTimeout(function(){
+	        getData()
+	    },3000);
+	}
 
-	/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-	*/
-	await adapter.setObjectNotExistsAsync('testVariable', {
-		type: 'state',
-		common: {
-			name: 'testVariable',
-			type: 'boolean',
-			role: 'indicator',
-			read: true,
-			write: true,
-		},
-		native: {},
+	schedule('*/30 * * * *', () => {
+	    getData();
+	});
+	
+	on({id: selectedCountryDP, change: "any"}, function (obj) {
+	    let value = obj.state.val;
+	    getData()
+	});
+	on({id: foodJSONDP, change: "any"}, function (obj) {
+	    let value = obj.state.val;
+	    setHTML(value)
 	});
 
-	// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-	adapter.subscribeStates('testVariable');
-	// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-	// adapter.subscribeStates('lights.*');
-	// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-	// adapter.subscribeStates('*');
-
-	/*
-		setState examples
-		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-	*/
-	// the variable testVariable is set to true as command (ack=false)
-	await adapter.setStateAsync('testVariable', true);
-
-	// same thing, but the value is flagged "ack"
-	// ack should be always set to true if the value is received from or acknowledged from the target system
-	await adapter.setStateAsync('testVariable', { val: true, ack: true });
-
-	// same thing, but the state is deleted after 30s (getState will return null afterwards)
-	await adapter.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-	// examples for the checkPassword/checkGroup functions
-	adapter.checkPassword('admin', 'iobroker', (res) => {
-		adapter.log.info('check user admin pw iobroker: ' + res);
-	});
-
-	adapter.checkGroup('admin', 'admin', (res) => {
-		adapter.log.info('check group user admin group admin: ' + res);
-	});
 }
 
-if (require.main !== module) {
-	// Export startAdapter in compact mode
-	module.exports = startAdapter;
+function setHTML(data){
+    let html = ''
+    let items = JSON.parse(data)
+    html += '<html><table style="width:100%; overflow:scroll;border-collapse: collapse; border: 2px solid black;">'
+    html += '<thead><tr>'
+    html += '<td style="border: 1px solid black;">Veröffentlicht</td>'
+    html += '<td style="border: 1px solid black;">Bild</td>'
+    html += '<td style="border: 1px solid black;">Titel</td>'
+    html += '<td style="border: 1px solid black;">Grund</td>'
+    html += '<td style="border: 1px solid black;">Hersteller</td>'
+    html += '</tr></thead>'
+    for(let i=0;i<items.length;i++){
+        html += '<tr style="border: 1px solid black;">';
+        
+        html += '<td style="width:15%;border: 1px solid black;">';
+        html += formatDate(items[i].pubDateTS,'DD.MM.YYYY hh:mm')
+        html += '</td>'
+
+        html += '<td style="text-align:center; width:20%;border: 1px solid black;">';
+        html += items[i].picture
+        html += '</td>'
+
+        html += '<td style="width:20%;border: 1px solid black;">';
+        html += items[i].title
+        html += '</td>'
+
+        html += '<td style="width:20%;border: 1px solid black;">';
+        html += items[i].reason
+        html += '</td>'
+
+        html += '<td style="width:10%;border: 1px solid black;">';
+        html += items[i].manufacturer
+        html += '</td>'
+
+        html += '</tr>'            
+    }
+    html += '</table><html>'
+    setState(foodHTMLDP,html)
+}
+
+function getData(){
+    //let parseString = require('xml2js').parseString;
+    let jsonCount = 0;    
+    httpGet(foodWarningRSS, function (error, response) {
+        if (!error && response.statusCode == 200) {
+    
+            parseString(response.data, {
+                explicitArray: false,
+                mergeAttrs: true
+            },
+            function (err, result) {
+                //log(JSON.stringify(result, null, 2));
+
+                if (err) {
+                    log("Fehler: " + err, 'error');
+                } else {    
+                    //var tabelle;
+                    let jsonWarning =[];
+                        // Titel links, Inhalt rechts
+                        //log(result.rss.channel);
+                    
+                    for (var i = 0; i <result.rss.channel.item.length; i++) {
+                        let item =result.rss.channel.item[i];
+                        let itemObj = {}
+                        // title, link, description (img src), 
+                        itemObj.pubDate = result.rss.channel.item[i].pubDate;
+                        itemObj.pubDateTS = new Date(itemObj.pubDate).getTime()
+                        //itemObj.guid = result.rss.channel.item[i].guid;
+                        let link = result.rss.channel.item[i].link;
+                        itemObj.title = result.rss.channel.item[i].title;
+                        httpGet(link, function (error, response) {
+                            //console.log(response.data)
+                            let item = response.data;
+                            //const html2json = require('html2json').html2json;
+                            //let itemJson = html2json(item);
+                            //console.log(JSON.stringify(itemJson.child[0].child[3].child[1].child))
+                            // html
+                            
+                            let pos = item.indexOf('lmw-section__head')
+                            let href = item.indexOf('href="',pos+1)+6
+                            let hrefEnd = item.indexOf('"',href+6)
+                            
+                            itemObj.picture = '<a href="'+link+'">'+'<img src="https://www.lebensmittelwarnung.de' + item.substring(href, hrefEnd) + '" style="max-width: 250px; width: auto; height: 100px; " ></a>' //overflow: "scroll" max-width="200px!important" width="auto" height="150px"
+                        
+                            let first = item.indexOf('lmw-description-list__item--product-name',hrefEnd)
+                            
+                            first = item.indexOf('<p>',first)+3
+                            let end = item.indexOf('</p>',first+3)
+                            itemObj.description = item.substring(first, end);
+                            
+                            first = item.indexOf('lmw-description-list__item--production-date',end)
+                            if(first>=0){
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.productDate = item.substring(first, end);
+                            }
+                            first = item.indexOf('lmw-description-list__item--durability',end)
+                            if(first>=0){
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.productDurability = item.substring(first, end);
+                            }
+                            first = item.indexOf('lmw-description-list__item--packaging-unit',end);
+                            if(first>=0){
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.packingUnit = item.substring(first, end);
+                            }
+                            first = item.indexOf('lmw-description-list__item--batch-number',end);
+                            if(first>=0){
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.chargeNumber = item.substring(first, end);
+                            //jsonWarning.push(itemObj)
+                            }
+                            first = item.indexOf('lmw-description-list__item--further-marking',end);
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.extDescription = item.substring(first, end);
+
+                            first = item.indexOf('lmw-description-list__item--manufacturer',end)
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.manufacturer = item.substring(first, end);
+
+                            end = item.indexOf('lmw-section__content',end)
+                            first = item.indexOf('lmw-section-toggle-panel-ID-1',end);
+                            first = item.indexOf('<span',first)+5
+                            first = item.indexOf('>',first+5)+1
+                            end = item.indexOf('</span>',first+1)
+                            itemObj.reason = item.substring(first, end);
+
+                            first = item.indexOf('lmw-description-list__description',end)
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.information = item.substring(first, end);
+
+
+                            first = item.indexOf('lmw-section-toggle-head-ID-2',end)
+                            first = item.indexOf('<ul',first)+3
+                            end = item.indexOf('</ul>',first+3)
+                            //log(item.substring(first, end))
+                            let ul = item.substring(first, end)
+                            /*
+                            ul = ul.replace('<li class="lmw-list__item">','')
+                            ul = ul.replace('<span class="lmw-badge lmw-badge--dark lmw-badge--large">','')
+                            ul = ul.replace('</li>','')
+                            ul = ul.replace('<ul class="lmw-list lmw-list--badges">','')
+                            console.log(ul);
+                            */
+                            const regex = /<li class="lmw-list__item">\s*<span class="lmw-badge lmw-badge--dark lmw-badge--large">([^<]+)<\/span>\s*<\/li>/g;
+                            let matches;
+                            let result = [];
+
+                            while ((matches = regex.exec(ul)) !== null) {
+                                result.push(matches[1]);
+                            }
+                            itemObj.listCountries = result;
+                            //const textContent = result.join(', ');
+                            //console.log(textContent);
+                            /*
+                            first = item.indexOf('lmw-description-list__description',end)
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.distribution = item.substring(first, end);
+
+                            first = item.indexOf('lmw-section--spring-wod',end);
+                            first = item.indexOf('<p>',first)+3
+                            end = item.indexOf('</p>',first+3)
+                            itemObj.distribution2 = item.substring(first, end);
+                            */
+                            let selectedCountry = adapter.config.federal_states;
+
+                            if(selectedCountry == '' || (selectedCountry != '' && result.includes(selectedCountry))) {
+                                jsonWarning.push(itemObj)
+                                
+                            }
+                        })
+                        
+                        if(i >= maxEntries-1){
+                            break;
+                        }
+                    }
+                    setTimeout(function(){
+                        let sortAscending = false;
+                        jsonWarning.sort((a, b) => sortAscending ? a.pubDateTS - b.pubDateTS : b.pubDateTS - a.pubDateTS);
+
+                        setState(foodJSONDP, JSON.stringify(jsonWarning));
+                    },1000);
+                }
+            });
+        } else  {
+            log(error, 'error');
+        }
+    });   // Ende request 
+}
+
+async function handleRequest() {
+    // create states
+    if (!objectsInitialized[warnings]) {
+	await createObjects();
+	objectsInitialized[warnings] = true;
+    }
+}
+
+async function createObjects() {
+    // create all Objects
+    adapter.log.debug(`Creating objects`);
+    await adapter.extendObjectAsync(user, {
+	type: 'state',
+	common: {name: 'warnings', role: 'state', type: 'boolean'}, //why boolean, it is a folder
+	native: {name: 'warnings'}
+    });
+
+    let obj = {
+        type: 'string',
+        common: {name: 'Lebensmittel-Warnungen JSON', read: true, write: false, role: 'text', type: 'string'},
+        native: {name: 'Lebensmittel-Warnungen JSON'}
+    };
+    await adapter.extendObjectAsync(`foodJSONDP`, obj);
+    obj = {
+        type: 'string',
+        common: {name: 'Lebensmittel-Warnungen HTML', read: true, write: false, role: 'text', type: 'string'},
+        native: {name: 'Lebensmittel-Warnungen HTML'}
+    };
+    await adapter.extendObjectAsync(`foodHTMLDP`, obj);	
+
+//createStateAsync(selectedCountryDP, {read: true, write: true, name: 'Gewähltes Land' , type: "string", role: "text", def: "", states: {}}); 
+
+}
+
+// If started as allInOne/compact mode => return function to create instance
+if (module && module.parent) {
+    module.exports = startAdapter;
 } else {
-	// otherwise start the instance directly
-	startAdapter();
+    // or start the instance directly
+    startAdapter();
 }
